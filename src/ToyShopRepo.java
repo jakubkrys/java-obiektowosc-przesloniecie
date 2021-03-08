99c76c29eed1069a5e971514fb74014695cca7a8
Jakub <jakubkrys@gmail.com>
2021-03-08 16:49:59 +0100

    #3 rozwinięcie zadania 1, dodanie puzzli oraz książek

--- src/ToyShopRepo.java
+++ src/ToyShopRepo.java
@@ -4,6 +4,10 @@ public class ToyShopRepo {
     private int bearIndex = 0;
     private Car[] cars = new Car[10];
     private int carIndex = 0;
+    private Puzzle[] puzzles = new Puzzle[10];
+    private int puzzleIndex = 0;
+    private Book[] books = new Book[10];
+    private int bookIndex = 0;
 
 
     public void addBear(Bear bear){
@@ -22,12 +26,34 @@ public class ToyShopRepo {
         }
     }
 
+    public void addPuzzle(Puzzle puzzle) {
+        if (puzzleIndex < puzzles.length) {
+            puzzles[puzzleIndex++] = puzzle;
+        } else {
+            puzzleIndex = 0;
+        }
+    }
+
+    public void addBook(Book book) {
+        if (bookIndex < books.length) {
+            books[bookIndex++] = book;
+        } else {
+            bookIndex = 0;
+        }
+    }
+
     public void displayAll(){
         for (int i = 0; i < bearIndex; i++) {
             bears[i].display();
         }
         for (int i = 0; i < carIndex; i++) {
             cars[i].display();
+        }
+        for (int i = 0; i < puzzleIndex; i++) {
+            puzzles[i].display();
+        }
+        for (int i = 0; i < bookIndex; i++) {
+            books[i].display();
         }
     }
 }
